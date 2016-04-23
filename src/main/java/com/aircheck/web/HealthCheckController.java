c0d3b881c1a1b9a8be672549e1d641d26e5f1c1f
timb123 <timb_123@yahoo.co.uk>
2016-04-23 14:13:59 +1000

    health update

--- src/main/java/com/aircheck/web/HealthCheckController.java
+++ src/main/java/com/aircheck/web/HealthCheckController.java
@@ -1,8 +1,26 @@
 package com.aircheck.web;
 
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.aircheck.model.SicknessDetail;
 
 @RestController
 public class HealthCheckController {
 
+	
+	@RequestMapping("/getHealthInfo")
+	public List<SicknessDetail> getHealthInfo() {
+		List<SicknessDetail> details = new ArrayList<>();
+		
+		return null;
+	}
+	
+//	public SicknessDetail detail = new SicknessDetail();
+	
 }
