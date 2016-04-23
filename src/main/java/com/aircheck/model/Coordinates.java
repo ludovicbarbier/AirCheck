c810b3a805102da551b9a1987692154267f3f1ac
timb123 <timb_123@yahoo.co.uk>
2016-04-23 15:37:53 +1000

    added dummy data

--- src/main/java/com/aircheck/model/Coordinates.java
+++ src/main/java/com/aircheck/model/Coordinates.java
@@ -1,6 +1,28 @@
 package com.aircheck.model;
 
 public class Coordinates {
-	String latitude;
-	String longitude;
+	String lat;
+	String lon;
+	String elev;
+	
+	public String getLat() {
+		return lat;
+	}
+	public void setLat(String lat) {
+		this.lat = lat;
+	}
+	public String getLon() {
+		return lon;
+	}
+	public void setLon(String lon) {
+		this.lon = lon;
+	}
+	public String getElev() {
+		return elev;
+	}
+	public void setElev(String elev) {
+		this.elev = elev;
+	}
+	
+	
 }
