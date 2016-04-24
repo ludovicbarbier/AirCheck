2e824bb1f58f3ace550c62c9dc348ec16a0d0d60
Leon Kelly <leon@naala.com.au>
2016-04-24 15:49:42 +1000

    Symptom filters

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,140 +1,3 @@
-$(function () {
-
-    //// TODO: This can be removed once we have some real data
-    //
-    ///*
-    // Symptom source
-    // */
-    //var features = [];
-    //var geoJSONSource;
-    //var clusterSource;
-    //var styleCache = {};
-    //var clusters;
-    //
-    //var image = new ol.style.Circle({
-    //    radius: 5,
-    //    fill: null,
-    //    stroke: new ol.style.Stroke({color: 'red', width: 1})
-    //});
-    //
-    //$.ajax({
-    //    method: "GET",
-    //    url: "sample/sample.json"
-    //}).done(function (data) {
-    //    geoJSONSource = new ol.source.Vector({
-    //        features: (new ol.format.GeoJSON()).readFeatures(data),
-    //        style: new ol.style.Style({
-    //            image: image
-    //        })
-    //    });
-    //    clusterSource = new ol.source.Cluster({
-    //        distance: 40,
-    //        source: geoJSONSource
-    //    });
-    //    clusters = new ol.layer.Vector({
-    //        source: clusterSource,
-    //        style: function (feature) {
-    //            var size = feature.get('features').length;
-    //            var style = styleCache[size];
-    //            if (!style) {
-    //                style = [new ol.style.Style({
-    //                    image: new ol.style.Circle({
-    //                        radius: 10,
-    //                        stroke: new ol.style.Stroke({
-    //                            color: '#fff'
-    //                        }),
-    //                        fill: new ol.style.Fill({
-    //                            color: '#3399CC'
-    //                        })
-    //                    }),
-    //                    text: new ol.style.Text({
-    //                        text: size.toString(),
-    //                        fill: new ol.style.Fill({
-    //                            color: '#fff'
-    //                        })
-    //                    })
-    //                })];
-    //                styleCache[size] = style;
-    //            }
-    //            return style;
-    //        }
-    //    });
-    //    clusters.setZIndex(999);
-    //    /*
-    //     WMTS source
-    //     */
-    //    var mapSource = new ol.source.WMTS({
-    //        url: "//map1{a-c}.vis.earthdata.nasa.gov/wmts-geo/wmts.cgi?TIME=2016-04-23",
-    //        layer: "MODIS_Terra_CorrectedReflectance_TrueColor",
-    //        format: "image/jpeg",
-    //        matrixSet: "EPSG4326_250m",
-    //        projection: ol.proj.get("EPSG:4326"),
-    //        tileGrid: new ol.tilegrid.WMTS({
-    //            origin: [-180, 90],
-    //            resolutions: [
-    //                0.5625,
-    //                0.28125,
-    //                0.140625,
-    //                0.0703125,
-    //                0.03515625,
-    //                0.017578125,
-    //                0.0087890625,
-    //                0.00439453125,
-    //                0.002197265625
-    //            ],
-    //            matrixIds: [0, 1, 2, 3, 4, 5, 6, 7, 8],
-    //            tileSize: 512
-    //        })
-    //    });
-    //
-    //    var mapLayer = new ol.layer.Tile({
-    //        source: mapSource
-    //    });
-    //    mapLayer.setZIndex(1);
-    //
-    //    /*
-    //     KML source for earthdata
-    //     */
-    //
-    //    var earthdata = new ol.layer.Vector({
-    //        source: new ol.source.Vector({
-    //            url: 'https://firms.modaps.eosdis.nasa.gov/active_fire/c6/kml/MODIS_C6_Australia_and_New_Zealand_24h.kml',
-    //            format: new ol.format.KML()
-    //        })
-    //    });
-    //    earthdata.setZIndex(100);
-    //
-    //    /*
-    //     Get user location
-    //     */
-    //
-    //    function getLocation() {
-    //        if (navigator.geolocation) {
-    //            navigator.geolocation.getCurrentPosition(showPosition);
-    //        } else {
-    //            alert("This application is not supported by this browser.");
-    //        }
-    //    }
-    //
-    //    function showPosition(position) {
-    //        var map = new ol.Map({
-    //            layers: [mapLayer, clusters],
-    //            target: 'map',
-    //            view: new ol.View({
-    //                center: [position.coords.longitude, position.coords.latitude],
-    //                zoom: 3
-    //            }),
-    //            renderer: ["canvas", "dom"]
-    //        });
-    //        map.getView().setCenter(ol.proj.transform([position.coords.longitude, position.coords.latitude], 'EPSG:4326', 'EPSG:3857'));
-    //    }
-    //
-    //    getLocation();
-    //});
-
-
-});
-
 window.onload = function () {
 
     var map = L.map("map", {
@@ -153,24 +16,79 @@ window.onload = function () {
         transparent: true
     }).addTo(map);
 
-
-
     map.addLayer(layer);
+
+    var symptoms = [];
+    var symptomsSelected = [];
 
     $.ajax({
         method: "GET",
         url: "sample/sample.json"
     }).done(function (data) {
         // L.geoJson(data).addTo(map);
+        // console.log(data.features[0].properties.SYMPTOMS)
 
-        var markers = L.markerClusterGroup();
-        var geoJsonLayer = L.geoJson(data, {
-            onEachFeature: function (feature, layer) {
-                layer.bindPopup(feature.properties.address);
+        data.features.forEach(function(feature) {
+            var symptom = feature.properties.SYMPTOMS;
+            if(symptoms.indexOf(symptom) < 0) {
+                symptoms.push(symptom);
             }
+        })
+
+        Array.prototype.push.apply(symptomsSelected, symptoms);
+        symptoms.forEach(function(symptom) {
+            $('#sympton-list').append('<div class="col-md-6 col-sm-6 col-lg-4"><label class="list-group-item"><input type="checkbox" checked name="' + symptom + '" value="' + symptom + '">' + symptom + '</label></div>');
+        })
+
+        $('#sympton-list').find('input').change(function(event){
+            var checked=event.currentTarget.checked;
+            var symptom=event.currentTarget.value;
+            if(checked) {
+                if(symptomsSelected.indexOf(symptom) < 0) {
+                    symptomsSelected.push(symptom);
+                }
+            } else {
+                var index = symptomsSelected.indexOf(symptom);
+                if(index > -1) symptomsSelected.splice(index, 1);
+            }
+
+        })
+
+
+
+        $('#btnUpdateMap').click(function(){
+
+            map.removeLayer(markers)
+            var drawingData = $.extend(true, {}, data);
+
+            for(var i = drawingData.features.length-1; i >= 0; i--) {
+                var feature = drawingData.features[i];
+                var symptom = feature.properties.SYMPTOMS;
+                if(symptomsSelected.indexOf(symptom) < 0) {
+                    var index = drawingData.features.indexOf(feature);
+                    if(index > -1) drawingData.features.splice(index, 1);
+                }
+            }
+            markers=drawMap(drawingData);
         });
-        markers.addLayer(geoJsonLayer);
-        map.addLayer(markers);
+
+        function drawMap(drawData) {
+            var markers = L.markerClusterGroup();
+            var geoJsonLayer = L.geoJson(drawData, {
+                onEachFeature: function (feature, layer) {
+                    layer.bindPopup(feature.properties.address);
+                }
+            });
+            markers.addLayer(geoJsonLayer);
+            map.addLayer(markers);
+
+            var aqi = L.tileLayer('http://tiles.aqicn.org/tiles/usepa-pm25/{z}/{x}/{y}.png?token=84265a59e803ec10370a06e96898bebef471d30b').addTo(map);
+            map.addLayer(aqi);
+
+            return markers;
+        }
+
+        var markers = drawMap(data);
 
     });
 };
